e845a7105491a6902abf670168b71fbdd23874f9
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-07-25 15:50:55 -0500

    add a task when click and the input lenght is not 0

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#new-task input").value.length == 0) {
     alert("Enter a Task c:");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+            <div id="tasks">
+                <span id="taskName">
+                ${document.querySelector("#new-task input").value}
+                </span>
+                <button class="delete">
+                </button>
+            </div>
+    `;
   }
 };
